3e1153323d22b12aebb1c276f5c02812c3abd9da
byference <1170126481@qq.com>
2019-03-10 19:18:56 +0800

    :sparkles: add spring-boot-profile module

---
+++ spring-boot-profile/spring-boot-profile/src/main/java/com/github/byference/profile/ProfileApplication.java
@@ -0,0 +1,17 @@
+package com.github.byference.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bfh
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
+
+}
