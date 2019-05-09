584f3ebfd02945ef85b6f94e290f6bce9f4c0b70
byference <1170126481@qq.com>
2019-05-09 21:15:46 +0800

    add valVariableTest

--- others/java-project/src/main/java/com/github/byference/lombok/LombokTest.java
+++ others/java-project/src/main/java/com/github/byference/lombok/LombokTest.java
@@ -1,7 +1,10 @@
 package com.github.byference.lombok;
 
 import com.github.byference.common.entity.Person;
+import lombok.val;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * LombokTest
@@ -10,6 +13,20 @@ import org.junit.Test;
  * @since 2019/05/01
  */
 public class LombokTest {
+
+
+    @Test
+    public void valVariableTest() {
+
+        val person = new Person();
+        person.setId(100).setName("jack");
+        // person = null;
+        // Cannot assign a value to final variable 'person'
+
+        val list = new ArrayList<Person>();
+        list.add(person);
+        System.out.println(list);
+    }
 
 
     @Test
