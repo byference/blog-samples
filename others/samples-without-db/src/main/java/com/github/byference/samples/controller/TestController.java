d78ad1e0e4549b3024e477dfe9aaa419ac80c4d9
byference <1170126481@qq.com>
2019-08-16 20:14:16 +0800

    InitBinder

--- others/samples-without-db/src/main/java/com/github/byference/samples/controller/TestController.java
+++ others/samples-without-db/src/main/java/com/github/byference/samples/controller/TestController.java
@@ -8,15 +8,14 @@ import com.github.byference.samples.service.EchoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.MediaType;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -44,8 +43,23 @@ public class TestController {
     private String formatter;
 
 
+
+    @InitBinder
+    public void initWebDataBinder(WebDataBinder binder) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat(formatter);
+        sdf.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, false));
+    }
+
+
     /**
      * by {@link WebMvcAutoConfiguration.EnableWebMvcConfiguration#mvcConversionService}
+     * application.properties:
+     * <code>
+     *     spring.mvc.date-format = yyyy-MM-dd
+     * </code>
+     * or user {@link #initWebDataBinder(WebDataBinder)}
      *
      * @param date {@link Date}
      */
