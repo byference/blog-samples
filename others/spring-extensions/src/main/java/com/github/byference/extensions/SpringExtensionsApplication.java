954eca1321a5bfebea994896ad994465839ff1a9
byference <1170126481@qq.com>
2019-10-02 20:23:49 +0800

    spring-extensions module init

---
+++ others/spring-extensions/src/main/java/com/github/byference/extensions/SpringExtensionsApplication.java
@@ -0,0 +1,16 @@
+package com.github.byference.extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author byference
+ * @since 2019/10/02
+ */
+@SpringBootApplication
+public class SpringExtensionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExtensionsApplication.class, args);
+    }
+}
